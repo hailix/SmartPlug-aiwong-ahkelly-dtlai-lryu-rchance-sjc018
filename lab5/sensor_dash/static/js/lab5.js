8cbfd30887666b4d9ef996985ed12a6bc0d7ffc4
rtchance <rchance@ucsd.edu>
2018-03-23 04:35:35 -0700

    Honestly, I'm done now

--- lab5/sensor_dash/static/js/lab5.js
+++ lab5/sensor_dash/static/js/lab5.js
@@ -1,8 +1,9 @@
 function getData(sensor,count) {
- //console.log(count);     
+
+    
         document.getElementById("slidercontainer").style.display="block";
         document.getElementById("loadingMessage").style.display="none";
-// pulled this off of my homework, not quite right for this
+
 	fetch('/sensor/'+sensor+'?count='+count).then(response => response.json()).then(
         function(response) { 
 
@@ -13,9 +14,8 @@ function getData(sensor,count) {
 			table.innerHTML = "";
             
 
-             	//var row = document.createElement("tr");
                 var row = table.insertRow(-1);
-		var cell = document.createElement("td");
+		var cell = document.createElement("th");
 
                 if (sensor == "MCP") {
                   row.insertCell(-1).innerHTML="Voltage Level";
@@ -40,11 +40,8 @@ function getData(sensor,count) {
 
             for (var item in response["tmp"]) {
               
-        console.log(response["tmp"]);
-  //      console.log(item);
-     //   console.log(value);
-     //   console.log(typeof(value));
-    //    console.log(response["tmp"][0].celsius);
+ 
+
         row = document.createElement("tr");
         cell1 = document.createElement("td");
 
@@ -67,32 +64,17 @@ function getData(sensor,count) {
           row.insertCell(-1).innerHTML=response["tmp"][item].gate;
           table.appendChild(row);
 	}
-      //  cell1.appendChild(value);
-      //  row.appendChild(cell1);
-      //  table.appendChild(row);
-        //document.getElementById('data1').innerHTML=response["tmp"][0].celsius; 
-	       
+
          }
 	})
 }
 
-var clicked1 = function(name, count) {
-    console.log('hi');
-  fetch('/sensor/'+name+'?count='+count)
-    .then(response => response.json())
-    .then(function(response) {
-      // document.getElementById('bn').innerHTML=response['business_name'];
-      // document.getElementById('bt').innerHTML=response['business_type'];
-      // document.getElementById('mt').innerHTML=response['market_type'];
-      // document.getElementById('jd').innerHTML=response['job_to_be_done']
-      // document.getElementById('rm').innerHTML=response['revenue_model'];
-      
-      for (item in response) {
-        
-        document.createElement(item).item(0);
-        document.getElementById('data1').innerHTML=response['tmp'][0];
-      }
-    }
-  );
+function plantInfo() {
+
+  x = document.getElementById("plantWindow");
+  if(x.style.display=="none") {
+    x.style.display="block";
+  }
 }
+
 
